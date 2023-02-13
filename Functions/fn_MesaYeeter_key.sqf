a9a4c98c9babf48e060a0c508440db997035c74e
sweetn3ss <40582849+sweetn3ss@users.noreply.github.com>
2023-02-12 19:59:30 -0800

    initial commit
    initial push of files to repo

---
+++ Functions/fn_MesaYeeter_key.sqf
@@ -0,0 +1,22 @@
+// Bubble Shield Uptime
+[
+"bubbleCock",
+"SLIDER",
+"Bubble Shield Duration",
+["82nd Options","Bubble Shield Options"],
+[0,600,300,0],
+1,
+{},
+nil
+] call CBA_fnc_addSetting;
+// Bubble Shield "Failure" chance
+[
+"lmaoprob",
+"SLIDER",
+"Bubble Shield Fail Probability",
+["82nd Options","Bubble Shield Options"],
+[0,99,10,0],
+1,
+{},
+nil
+] call CBA_fnc_addSetting;
---
+++ config.cpp
@@ -0,0 +1,129 @@
+class CfgPatches
+{
+	class MM_CircularDefence {
+		name="82ndSOC Yeeter";
+		requiredAddons[]= {
+			"OPTRE_Weapons",
+			"OPTRE_FC_Weapons"
+		};
+		units[]={};
+		weapons[]={};
+		magazines[]={};
+		ammo[]={};
+		requiredVersion=0.1;
+	};
+};
+class Extended_PostInit_EventHandlers {
+    class MesaYeeter_Key {
+        init = "spawn esThrow_fnc_MesaYeeter_Key";
+    };
+};
+class cfgFunctions {
+	class esThrow {
+		class functions {
+			file="MM_CircularDefence\functions";
+			class bubbleshield_throw {};
+		};
+		class Keys {
+			file="\MM_CircularDefence\functions";
+			class MesaYeeter_Key {};
+		};
+	};
+};
+class cfgAmmo {
+	class ACE_SatchelCharge_Remote_Ammo_Thrown;
+	class 82nd_Bubbleshield_Ammo: ACE_SatchelCharge_Remote_Ammo_Thrown {
+		hit=0;
+		indirectHit=0;
+		indirectHitRange=0;
+		defaultMagazine="82nd_Bubble_Mag";
+		model="\MM_CircularDefence\data\bubble\bubbleprojectile.p3d";
+		explosive=0;
+		explosionType="";
+		mineModelDisabled="\MM_CircularDefence\data\bubble\bubbleprojectile.p3d";
+		triggerWhenDestroyed=0;
+		explosionEffects="";
+		CraterEffects="";
+		soundHit1[]={"",0,0,0};
+		multiSoundHit[]={};
+		mineInconspicuousness=9999;
+		timetolive=15;
+		explosionTime=6;
+		effectFlare="";
+		effectsFire="";
+		effectsMissile="";
+		effectsSmoke="";
+		explosionEffectsDir="";
+		explosionForceCoef="";
+		explosionPos="";
+		explosionEffectsRadius=0;
+		explosionSoundEffect="";
+		SoundSetExplosion[] ={""};
+		class CamShakeExplode {
+			power=0;
+			duration=0;
+			frequency=0;
+			distance=0;
+		};
+	};
+};
+class cfgMagazines {
+	class OPTRE_M9_Frag;
+	class 82nd_Bubble_Mag : OPTRE_M9_Frag {
+		scope=2;
+		scopeArsenal=2;
+		displayName="Z-4190 TPE/SS";
+		displayNameShort="Throwable Bubble Shield";
+		descriptionShort="Throwable Bubble Shield";
+		type="256";
+		allowedSlots[]={901,701};
+		ammo="82nd_Bubbleshield_Ammo";
+		model="OPTRE_Weapons\explosives\m2_smk_grenade.p3d";
+		mass=15;
+		count=1;
+		sound[]={"A3\sounds_f\dummysound",0.000316228,1,10};
+		initSpeed=12;
+	};
+};
+class cfgWeapons {
+	class Grenadelauncher;
+	class ThrowMuzzle;
+	class Throw: Grenadelauncher {
+		muzzles[]+= {
+			"82nd_Bubble_Muzzle",
+		};
+        class 82nd_Bubble_Muzzle: ThrowMuzzle {
+			reloadtime=0;
+			magazines[]={"82nd_Bubble_Mag"};
+			
+			class EventHandlers {
+				fired="_this call Throw_fnc_bubbleshield_throw;";
+			};
+		};
+	};
+};
+
+class cfgVehicles {
+	class ThingX;
+	class Items_base_F: ThingX {
+		class DestructionEffects;
+	};
+	class House;
+	class House_F: House {
+		class DestructionEffects;
+	};
+	class 82nd_Bubble_Shield: House_F {
+		scope=2;
+		scopeCurator=2;
+		armor=999999;
+		armorStructural=999;
+		displayName="[82ndSOC] Bubble Shield";
+		model="\MM_CircularDefence\data\bubble\bubbledeployed.p3d";
+		author="Saint";
+		editorCategory="82nd_Battalion";
+		editorSubcategory="82nd_Props";
+		class EventHandlers {
+			Dammaged = "_this call Throw_fnc_bubbleshield_hit;";
+		};
+	};
+};
